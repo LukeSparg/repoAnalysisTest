ca3b9061b0109a9bc9defd0fec9bb1c5f873b378
Luke Sparg <50121687+LukeSparg@users.noreply.github.com>
2021-06-08 17:59:40 -0700

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+//A comment
+public class Test {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+  /*Testing
+  Testing a block comment
+  Yay*/
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+
+// Outputs "I just got executed!"
