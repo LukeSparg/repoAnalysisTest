db5807f383ba10ad4108f08c65df9a31a9054a7d
Luke Sparg <50121687+LukeSparg@users.noreply.github.com>
2021-06-08 17:57:30 -0700

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#Testing comment
+
+testVariable = 1
+
+
+def testFunction:
+  """
+  A docstring for testing
+
+  5 lines total!
+  """
+  testVariableNumberTwo = 3
+  testVariableNumberTwo++
+  if testVariableNumberTwo == testVariable:
+    #Another Comment
+    testVariable = 3
+  
+  
