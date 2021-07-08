32c761c10a96ff800cf41ba0506054b16154e3d4
Devina Prasad Jaiswal <43189092+devinapj@users.noreply.github.com>
2021-07-07 21:50:02 -0700

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,3 +1,3 @@
-from math import *   
+import math 
 number = 2e-7  # small value of of x    
-print('log(fabs(x), base) is :', log(fabs(number), 10)) 
+print('log(fabs(x), base) is :', math.log(math.fabs(number), 10)) 
